9f9d054b80415b50c39538516542998463fbf67a
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-02 23:45:16 +0700

    Time: 0 ms (100.00%) | Memory: 6 MB (18.14%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,11 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    numsMap := make(map[int]int)
+    numMap := make(map[int]int)
     for i,num := range nums {
         findSecondTarget := target-num
-        if index, found := numsMap[findSecondTarget]; found {
+        if index, found := numMap[findSecondTarget]; found {
             return []int{index, i} 
         }
-        numsMap[num] = i
+        numMap[num] = i
     }
     return nil
 }
