824a5f8cf0b26a132f9243a2a6742bc7428499de
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-03 23:51:56 +0700

    Time: 0 ms (100.00%) | Memory: 5 MB (13.92%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.go
+++ 13-roman-to-integer/roman-to-integer.go
@@ -1,14 +1,5 @@
 func romanToInt(s string) int {
     romanMap := map[string]int{"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
-    // romanMap["I"] = 1
-    // romanMap["V"] = 5
-    // romanMap["X"] = 10
-    // romanMap["L"] = 50
-    // romanMap["C"] = 100
-    // romanMap["D"] = 500
-    // romanMap["M"] = 1000
-
-
     numIndex := make([]int, len(s))
     result := 0
     for index, num := range s {
