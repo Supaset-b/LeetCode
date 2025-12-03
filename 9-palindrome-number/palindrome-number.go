3cfbd3f9c2fa2ab5f633a9edc40bf5e443cff61e
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-03 23:04:57 +0700

    Time: 4 ms (50.18%) | Memory: 6 MB (77.18%) - LeetSync

--- 9-palindrome-number/palindrome-number.go
+++ 9-palindrome-number/palindrome-number.go
@@ -1,15 +1,13 @@
 func isPalindrome(x int) bool {
-    
     if x < 0 {
         return false
     }
-
-    original := x
-    reverse := 0
+    originalX := x
+    reverseX := 0
 
     for x != 0 {
-        reverse = (reverse*10) + (x %10) 
+        reverseX = (reverseX*10) + (x %10) 
         x = x/10
     }
-    return original == reverse
+    return originalX == reverseX
 }
