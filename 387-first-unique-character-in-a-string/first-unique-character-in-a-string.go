7f857d4f8a136ef01ff1d4a889ccc91dd897a9c6
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-06 11:57:00 +0700

    Time: 24 ms (50.77%) | Memory: 8.2 MB (26.47%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -1,5 +1,23 @@
 func firstUniqChar(s string) int {
 
+    // mappingRuneIndex := make(map[rune]int)
+    // mappingRuneBool := make(map[rune]bool)
+    
+    // for i, c := range s {
+    //     if _, value := mappingRuneIndex[c]; value{
+    //         mappingRuneBool[c] = true
+    //     } else {
+    //         mappingRuneBool[c] = false
+    //     }
+    //     mappingRuneIndex[c] = i
+    // }
+
+    // for index, value := range s {
+    //     if !mappingRuneBool[value] {
+    //         return index
+    //     }
+    // }
+
     mappingRuneIndex := make(map[rune]int)
     for _, c := range s {
         mappingRuneIndex[c]++
