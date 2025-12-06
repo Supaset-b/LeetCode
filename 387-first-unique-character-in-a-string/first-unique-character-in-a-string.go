2869823003af6df23eeb971a45b9a4a0600b6b02
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-06 11:56:23 +0700

    Time: 39 ms (16.56%) | Memory: 7.7 MB (89.63%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -1,34 +1,32 @@
 func firstUniqChar(s string) int {
 
-    // mappingRuneIndex := make(map[rune]int)
-    // mappingRuneBool := make(map[rune]bool)
-    
-    // for i, c := range s {
-    //     if _, value := mappingRuneIndex[c]; value{
-    //         mappingRuneBool[c] = true
-    //     } else {
-    //         mappingRuneBool[c] = false
-    //     }
-    //     mappingRuneIndex[c] = i
-    // }
-
-    // for index, value := range s {
-    //     if !mappingRuneBool[value] {
-    //         return index
-    //     }
-    // }
-
     mappingRuneIndex := make(map[rune]int)
-    for _, c := range s {
-        mappingRuneIndex[c]++
+    mappingRuneBool := make(map[rune]bool)
+    
+    for i, c := range s {
+        if _, value := mappingRuneIndex[c]; value{
+            mappingRuneBool[c] = true
+        } else {
+            mappingRuneBool[c] = false
+        }
+        mappingRuneIndex[c] = i
     }
 
-    for i, c := range s {
-        if mappingRuneIndex[c] == 1 {
-            return i
+    for index, value := range s {
+        if !mappingRuneBool[value] {
+            return index
         }
     }
 
-    
+    // mappingRuneIndex := make(map[rune]int)
+    // for _, c := range s {
+    //     mappingRuneIndex[c]++
+    // }
+
+    // for i, c := range s {
+    //     if mappingRuneIndex[c] == 1 {
+    //         return i
+    //     }
+    // }
     return -1
 }
