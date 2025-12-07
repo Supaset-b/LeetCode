41ffac7d894888dbc331d90585d02773d3b686ca
Supaset-b <108315445+Supaset-b@users.noreply.github.com>
2025-12-07 23:49:17 +0700

    Time: 539 ms (5.01%) | Memory: 105.1 MB (5.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.go
+++ 125-valid-palindrome/valid-palindrome.go
@@ -6,24 +6,16 @@ func isPalindrome(s string) bool {
             newS += string(char)
         }
     }
-    // newS = strings.ToLower(newS)
+    newS = strings.ToLower(newS)
     
     i := 0
     j := len(newS)-1
     for i < j {
-        if toLower(newS[i]) != toLower(newS[j]) {
+        if newS[i] != newS[j] {
             return false
         } 
         i ++
         j --
     }
     return true
-}
-
-func toLower(b byte) byte {
-    // fast ASCII lowercase
-    if b >= 'A' && b <= 'Z' {
-        return b + 32
-    }
-    return b
 }
