b9a4ccd0d5f0fd059b8b44830206f9686e303925
Supaset Buakaew <108315445+Supaset-b@users.noreply.github.com>
2025-12-13 14:56:15 +0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (51.33%) - LeetSync

--- 136-single-number/single-number.go
+++ 136-single-number/single-number.go
@@ -1,16 +1,20 @@
 func singleNumber(nums []int) int {
-    m := make(map[int]bool)
-    for _,num := range nums{
-        if m[num] {
-            m[num] = false
-        } else {
-            m[num] = true
-        }
+    // m := make(map[int]bool)
+    // for _,num := range nums{
+    //     if m[num] {
+    //         m[num] = false
+    //     } else {
+    //         m[num] = true
+    //     }
+    // }
+    // for k,v := range m {
+    //     if v {
+    //         return k
+    //     }
+    // }
+    ret := 0
+    for _,num := range nums {
+        ret ^= num
     }
-    for k,v := range m {
-        if v {
-            return k
-        }
-    }
-    return 0
+    return ret
 }
