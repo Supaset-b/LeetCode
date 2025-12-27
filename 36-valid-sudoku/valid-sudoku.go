a88dc998e5482a7e6f963cf4049c90c248454b41
Supaset Buakaew <108315445+Supaset-b@users.noreply.github.com>
2025-12-27 12:23:37 +0700

    Time: 0 ms (100.00%) | Memory: 4.8 MB (63.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,8 +1,5 @@
 func isValidSudoku(board [][]byte) bool {
-
-
     box := make([][]byte, 9)
-
     // chcek column and row
     for i:=0; i<9; i++ {
         mapCheck := make(map[byte]bool)
@@ -31,35 +28,8 @@ func isValidSudoku(board [][]byte) bool {
 }
 
 func storeBox(column int, row int, num byte, box *[][]byte) {
-    
     boxIndex := (row/3)*3 + (column/3)
     (*box)[boxIndex] = append((*box)[boxIndex], num)
-
-    // if 0 <= row && row <= 2 {
-    //     if 0 <= column && column <= 2 {
-    //         (*box)[0] = append((*box)[0], num)
-    //     } else if 3 <= column && column <= 5 {
-    //         (*box)[1] = append((*box)[1], num)
-    //     } else if 6 <= column && column <= 9 {
-    //         (*box)[2] = append((*box)[2], num)
-    //     }
-    // } else if 3 <= row && row<= 5 {
-    //     if 0 <= column && column <= 2 {
-    //         (*box)[3] = append((*box)[3], num)
-    //     } else if 3 <= column && column <= 5 {
-    //         (*box)[4] = append((*box)[4], num)
-    //     } else if 6 <= column && column <= 9 {
-    //         (*box)[5] = append((*box)[5], num)
-    //     }
-    // } else if 6 <= row && row <= 9 {
-    //     if 0 <= column && column <= 2 {
-    //         (*box)[6] = append((*box)[6], num)
-    //     } else if 3 <= column && column <= 5 {
-    //         (*box)[7] = append((*box)[7], num)
-    //     } else if 6 <= column && column <= 9 {
-    //         (*box)[8] = append((*box)[8], num)
-    //     }
-    // }
 }
 
 func checkNumBox(box [][]byte) bool{
