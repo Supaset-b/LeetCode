c90e8ba5bdcff44c3b0b844e2897c7dfccfaccce
Supaset Buakaew <108315445+Supaset-b@users.noreply.github.com>
2026-02-16 19:15:24 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (33.48%) - LeetSync

--- 190-reverse-bits/reverse-bits.go
+++ 190-reverse-bits/reverse-bits.go
@@ -1,29 +1,4 @@
 func reverseBits(n int) int {
-    // reverseN := 0
-    // index := -1
-    // for n != 1 {
-    //     // increase index by 1
-    //     index++
-
-    //     // % mod for get num
-    //     b := n % 2
-        
-    //     // reverse bit
-    //     if b == 0 {
-    //         b = 1
-    //         // convert bit to nums
-    //         reverseN += int(math.Pow( float64(2), float64(index)))
-    //     } else {
-    //         b = 0
-    //     }
-
-    //     // / divide for continue next
-    //     n = n/2
-    // }
-
-    // return reverseN
-    
-    // create string fill 0 for 32 bits
     reverseN := 0
     for i:=31; i>=0; i-- {
         b := n%2
