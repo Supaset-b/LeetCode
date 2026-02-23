3435ef0b002470b5a1803931a6a255ed5098925d
Supaset Buakaew <108315445+Supaset-b@users.noreply.github.com>
2026-02-23 20:53:14 +0700

    Time: 6 ms (26.77%) | Memory: 7.9 MB (39.37%) - LeetSync

--- 506-relative-ranks/relative-ranks.go
+++ 506-relative-ranks/relative-ranks.go
@@ -4,20 +4,23 @@ func findRelativeRanks(score []int) []string {
     sortScore := make([]int, lengthScore)
     copy(sortScore, score)
     slices.Sort(sortScore)
-    for i,s := range score {
-        for j,n:= range sortScore {
-            if s == n {
-                switch j {
-                    case lengthScore-1:
-                        rankScore[i] = "Gold Medal"
-                    case lengthScore-2:
-                        rankScore[i] = "Silver Medal"
-                    case lengthScore-3:
-                        rankScore[i] = "Bronze Medal"
-                    default:
-                        rankScore[i] = strconv.Itoa(lengthScore-j)
-                }
-            }
+    mappingSort := make(map[int]int)
+    for j,n:= range sortScore {
+        mappingSort[n] = j
+    }
+
+    // loop for checking ranks
+    for i,s := range score { 
+        rank := lengthScore - mappingSort[s]
+        switch mappingSort[s] {
+            case lengthScore-1:
+                rankScore[i] = "Gold Medal"
+             case lengthScore-2:
+                rankScore[i] = "Silver Medal"
+            case lengthScore-3:
+                rankScore[i] = "Bronze Medal"
+            default:
+                rankScore[i] = strconv.Itoa(rank)
         }
     }
     return rankScore
