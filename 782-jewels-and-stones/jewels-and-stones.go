2a8a5d5d8770d3d4aac411c78ac4021ddb5a73c4
Supaset Buakaew <108315445+Supaset-b@users.noreply.github.com>
2026-05-08 13:09:11 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (35.59%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.go
+++ 782-jewels-and-stones/jewels-and-stones.go
@@ -5,21 +5,6 @@ func numJewelsInStones(jewels string, stones string) int {
     }
 
     cnt := 0
-    // l := 0
-    // r := len(stones)-1
-    // for l<=r {
-    //     if m[rune(stones[l])] {
-    //         cnt++
-    //         if l==r {
-    //             break
-    //         }
-    //     }
-    //     if m[rune(stones[r])] {
-    //         cnt++
-    //     }
-    //     l++
-    //     r--
-    // }
     for _, s := range stones {
         if m[s] {
             cnt ++
